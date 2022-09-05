a393fe89afcc38e97770e9420f34c18d7f433fbb
escowin <edwin@escowinart.com>
2022-09-05 17:19:15 -0500

    + <Gallery>

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
 function App() {
   return (
     <div id='body' className='flex-column'>
       <Nav/>
-      <main></main>
+      <main>
+        <Gallery/>
+      </main>
       <About/>
     </div>
   );
---
+++ src/components/Gallery/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Gallery() {
+    return (
+        <section>
+            <h1>name</h1>
+            <p>description</p>
+        </section>
+    );
+}
+
+export default Gallery;
