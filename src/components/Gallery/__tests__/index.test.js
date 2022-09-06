fc63dcb49f63246d039a20c363e25a20098a41c9
escowin <edwin@escowinart.com>
2022-09-06 13:23:39 -0500

    tests | Gallery, see note js:5

---
+++ src/components/Gallery/__tests__/index.test.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+import Gallery from '..';
+// pause 20.3.6
--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,39 +1,67 @@
 import React from "react";
 import { render, cleanup } from "@testing-library/react";
-import '@testing-library/jest-dom/extend-expect';
-import Nav from '..';
+import "@testing-library/jest-dom/extend-expect";
+import Nav from "..";
 
-describe('Nav compnent', () => {
-    // baseline test
-    it('renders', () => {
-        render(<Nav/>);
-    });
+const categories = [{ name: "volume-one", description: "original run" }];
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
 
-    // snapshot test
-    it('matches snapshot', ()  => {
-        const { asFragment } = render(<Nav/>);
-        // assert value comparison
-        expect(asFragment()).toMatchSnapshot();
-    });
+describe("Nav compnent", () => {
+  // baseline test
+  it("renders", () => {
+    render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+      />
+    );
+  });
+
+  // snapshot test
+  it("matches snapshot", () => {
+    const { asFragment } = render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+      />
+    );
+    // assert value comparison
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
 
-describe('a element is visible', () => {
-    it('is nested within h2', () => {
-        // arrange
-        const { getByLabelText } = render(<Nav/>);
-        // assert
-        expect(getByLabelText('title')).toHaveTextContent('Bluestrawberry');
-    });
+describe("a element is visible", () => {
+  it("is nested within h2", () => {
+    // arrange
+    const { getByLabelText } = render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+      />
+    );
+    // assert
+    expect(getByLabelText("title")).toHaveTextContent("Bluestrawberry");
+  });
 });
 
-describe('nav links are visible', () => {
-    it('inserts text into nav links', () => {
-        // arrange
-        const { getByTestId } = render(<Nav/>);
-        // expect
-        expect(getByTestId('link')).toHaveTextContent('Bluestrawberry');
-        expect(getByTestId('about')).toHaveTextContent('About');
-    })
-})
+describe("nav links are visible", () => {
+  it("inserts text into nav links", () => {
+    // arrange
+    const { getByTestId } = render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+      />
+    );
+    // expect
+    expect(getByTestId("link")).toHaveTextContent("Bluestrawberry");
+    expect(getByTestId("about")).toHaveTextContent("About");
+  });
+});
 
 afterEach(cleanup);
